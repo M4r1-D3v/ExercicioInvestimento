d5dbc4e74e2673a8134cb5a29b75cd2a1e489c65
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-11-04 14:18:42 -0300

    Criar lista de investimento

--- src/main/java/br/com/zup/Investimento/InvestimentoService.java
+++ src/main/java/br/com/zup/Investimento/InvestimentoService.java
@@ -1,9 +1,14 @@
 package br.com.zup.Investimento;
 
+import br.com.zup.Investimento.dtos.InvestimentoInicialDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class InvestimentoService {
+    private List<InvestimentoInicialDTO>investimentoInicialDTOS = new ArrayList<>();
 
 
 }
