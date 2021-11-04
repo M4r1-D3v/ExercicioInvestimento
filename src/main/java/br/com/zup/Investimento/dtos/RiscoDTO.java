55648242ce2a3d89c73f7d47b94ab5e0a282bd19
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-11-04 15:02:46 -0300

    Ajustar classe RiscoDTO

--- src/main/java/br/com/zup/Investimento/dtos/RiscoDTO.java
+++ src/main/java/br/com/zup/Investimento/dtos/RiscoDTO.java
@@ -1,7 +1,15 @@
 package br.com.zup.Investimento.dtos;
 
 public enum RiscoDTO {
-    ALTO, MEDIO, BAIXO;
+    ALTO (0.25), MEDIO(0.15), BAIXO(0.005);
 
+    private double taxaRetorno;
 
+    RiscoDTO(double taxaRetorno) {
+        this.taxaRetorno = taxaRetorno;
+    }
+
+    public double getTaxaRetorno() {
+        return taxaRetorno;
+    }
 }
